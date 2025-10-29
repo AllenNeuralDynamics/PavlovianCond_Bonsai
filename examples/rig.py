b08c35268e2f0105cbf3f3182148d3c5c6387cd9
RoboDoig <erskine94@gmail.com>
2025-10-29 13:36:43 +0000

    Add deploy script

---
+++ examples/rig.py
@@ -0,0 +1,11 @@
+import datetime
+import os
+
+import aind_behavior_services.rig as rig
+from aind_behavior_pavlovian_conditioning.rig import (
+    AindPavlovianConditioningRig
+)
+
+rig = AindPavlovianConditioningRig(
+
+)
--- src/aind_behavior_pavlovian_conditioning/rig.py
+++ src/aind_behavior_pavlovian_conditioning/rig.py
@@ -11,9 +11,9 @@ import aind_behavior_services.calibration.water_valve as wvc
 import aind_behavior_services.rig as rig
 from pydantic import BaseModel, Field
 
-# from aind_behavior_pavlovian_conditioning import __semver__
+from aind_behavior_pavlovian_conditioning import __semver__
 
 class AindPavlovianConditioningRig(rig.AindBehaviorRigModel):
-    # version: Literal[__semver__] = __semver__
+    version: Literal[__semver__] = __semver__
     harp_behavior: rig.harp.HarpBehavior = Field(..., description="Harp behavior")
     harp_sound_card: rig.harp.HarpSoundCard = Field(..., description="Harp sound card")
