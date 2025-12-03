265a9fd8c47db4abd9e828f669b985ea10d2d88d
RoboDoig <erskine94@gmail.com>
2025-12-03 11:05:26 +0000

    Example launcher with parameters

--- scripts/launcher.ps1
+++ scripts/launcher.ps1
@@ -1,2 +1,8 @@
-$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location -Path (Split-Path -Parent $scriptPath)
+# $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+# Set-Location -Path (Split-Path -Parent $scriptPath)
+param([string]$SchemaFolder=".\examples")
+
+.\.venv\Scripts\Activate.ps1
+&python $SchemaFolder\session.py
+&python $SchemaFolder\rig.py
+&python $SchemaFolder\task.py
